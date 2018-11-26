c632813a9825079601edbf19c853a49785c6c184
SurekhaBeri <45308417+SurekhaBeri@users.noreply.github.com>
2018-11-26 12:33:00 +0530

    Update sva.java

--- sva.java
+++ sva.java
@@ -1,8 +1,7 @@
-package com.sanumala;
 public class HelloWorld
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello gist @ GitHub!");   
+         System.out.println("Hello World!!!");   
       }
 } 
